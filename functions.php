a82112f64d12c8c18f78565731c177fb9b662add
longvt93 <vutuanlongdhcn@gmail.com>
2021-09-06 11:10:47 +0700

    Không bỏ gutenberg

--- functions.php
+++ functions.php
@@ -31,7 +31,7 @@ if ( ! function_exists( 'khoinguyen_setup' ) ) :
 
 		// Add default posts and comments RSS feed links to head.
 		add_theme_support( 'automatic-feed-links' );
-		add_filter( 'use_block_editor_for_post', '__return_false' );
+		// add_filter( 'use_block_editor_for_post', '__return_false' );
 		/*
 		 * Let WordPress manage the document title.
 		 * By adding theme support, we declare that this theme does not use a
