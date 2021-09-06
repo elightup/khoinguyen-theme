ad9eba3449aa7aaef52d6a060caaaa14c3d9e0cc
le <leung01628381807@gmail.com>
2021-09-06 17:31:07 +0700

    update pase single

--- template-parts/content-product.php
+++ template-parts/content-product.php
@@ -1,7 +1,8 @@
 <?php
 $terms = get_the_terms(get_the_ID(), 'nganh-hang');
-$price=rwmb_meta('price',get_the_ID());
-$priceCV=rwmb_meta('price_nhap',get_the_ID());
+$price = rwmb_meta('price', get_the_ID());
+$priceCV = rwmb_meta('price_nhap', get_the_ID());
+$Qtang = rwmb_meta('gift', get_the_ID());
 $class      = '';
 
 if ($terms) {
@@ -11,25 +12,33 @@ if ($terms) {
 }
 ?>
 
-	<article class="product-item shows <?php echo $class;?>">
-		<div class="product-thumb">
-			<a class="post-thumbnail" href="<?php the_permalink(); ?>">
-				<?php the_post_thumbnail(); ?>
-			</a>
-		</div>
-		<div class="product-meta">
-			<?php
-			kn_entry_title();
+<article class="product-item shows <?php echo $class; ?>">
+	<div class="product-thumb">
+		<a class="post-thumbnail" href="<?php the_permalink(); ?>">
+			<?php the_post_thumbnail(); ?>
+		</a>
+	</div>
+	<div class="product-meta">
+		<?php
+		kn_entry_title();
 
-			?>
-			<div class="product-price">
+		?>
+		<div class="product-price">
 
-				<p class="price-sale"><?php echo kn_currency_format( $priceCV?$priceCV:0) ?></p>
-				<p class="price"><?php echo kn_currency_format( $price?$price:0) ?></p>
-			</div>
-			<div class="product-khuyenmai">
-				<p>Bộ quà tặng lên đến 2.000.000Đ</p>
-			</div>
+			<p class="price-sale"><?php echo kn_currency_format($priceCV ? $priceCV : 0) ?></p>
+			<p class="price"><?php echo kn_currency_format($price ? $price : 0) ?></p>
 		</div>
-		<?php  kn_get_phantram($price?$price:0,$priceCV?$priceCV:0) ?>
-	</article>
+
+		<?php
+		if (empty($Qtang)) {
+			echo '<div class="product-khuyenmai-none"></div>';
+		} else {
+			echo '<div class="product-khuyenmai">';
+			echo '<p>' . $Qtang . '</p>';
+			echo '</div>';
+		}
+		?>
+	
+	</div>
+	<?php kn_get_phantram($price ? $price : 0, $priceCV ? $priceCV : 0) ?>
+</article>
