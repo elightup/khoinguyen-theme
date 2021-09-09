935cf673b765636d29fca51d731fe0a1cb940497
le <leung01628381807@gmail.com>
2021-09-09 14:13:21 +0700

    responsive

--- inc/shortcode.php
+++ inc/shortcode.php
@@ -51,40 +51,40 @@ function shortcode_get_banchay()
 
 	$query = new WP_Query($args);
 ?>
-	
-	<?php
-	while ($query->have_posts()) :
-		$query->the_post();
-		$price=rwmb_meta('price',get_the_ID());
-		$priceCV=rwmb_meta('price_nhap',get_the_ID());
-	?>
+	<div class="box_product_sidebar">
+		<?php
+		while ($query->have_posts()) :
+			$query->the_post();
+			$price = rwmb_meta('price', get_the_ID());
+			$priceCV = rwmb_meta('price_nhap', get_the_ID());
+		?>
 
-		<div class="product_sidebar">
-			<div class="product_sidebar-content">
-				<div class="product-img">
-					<a class="post-img" href="<?php the_permalink(); ?>">
-						<?php the_post_thumbnail(); ?>
-					</a>
+			<div class="product_sidebar">
+				<div class="product_sidebar-content">
+					<div class="product-img">
+						<a class="post-img" href="<?php the_permalink(); ?>">
+							<?php the_post_thumbnail(); ?>
+						</a>
+					</div>
+					<div class="product_sidebar-meta">
+						<?php
+						kn_entry_title();
+
+						?>
+					</div>
 				</div>
-				<div class="product_sidebar-meta">
-					<?php
-					kn_entry_title();
+				<div class="product_sidebar-price">
+					<p class="price"><?php echo kn_currency_format($price ? $price : 0) ?></p>
+					<p class="price-sale"><?php echo kn_currency_format($priceCV ? $priceCV : 0) ?></p>
 
-					?>
 				</div>
 			</div>
-			<div class="product_sidebar-price">
-				<p class="price"><?php echo kn_currency_format( $price?$price:0) ?></p>
-				<p class="price-sale"><?php echo kn_currency_format( $priceCV?$priceCV:0) ?></p>
-
-			</div>
-		</div>
-	<?php
-	endwhile;
-	wp_reset_postdata();
-	return ob_get_clean();
-	?>
-
+		<?php
+		endwhile;
+		wp_reset_postdata();
+		return ob_get_clean();
+		?>
+	</div>
 <?php
 }
 add_shortcode('get_banchay', 'shortcode_get_banchay');
