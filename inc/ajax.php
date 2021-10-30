beada019db7704d1c092e4c64201c24c806075a6
longvt93 <vutuanlongdhcn@gmail.com>
2021-10-30 10:01:11 +0700

    Chỉnh lại scrollbar của web

--- inc/ajax.php
+++ inc/ajax.php
@@ -5,16 +5,15 @@ function display_districts() {
 	if ( empty( $_POST['city'] ) ) {
 		wp_send_json_error();
 	}
-	$city_id = $_POST['city'];
+	$city_id   = $_POST['city'];
 	$districts = kn_get_districts_from_city( $city_id );
-	$response = [];
+	$response  = [];
 	foreach( $districts as $key => $district ) {
 		$response[] = [
 			$key => $district
 		];
 	}
 	wp_send_json_success( $response );
-
 }
 
 
@@ -25,13 +24,12 @@ function display_wards() {
 		wp_send_json_error();
 	}
 	$district_id = $_POST['district'];
-	$wards = kn_get_wards_from_district( $district_id );
-	$response = [];
+	$wards       = kn_get_wards_from_district( $district_id );
+	$response    = [];
 	foreach( $wards as $key => $ward ) {
 		$response[] = [
 			$key => $ward
 		];
 	}
 	wp_send_json_success( $response );
-
 }
