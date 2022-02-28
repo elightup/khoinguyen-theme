4c8b9dd4de60e8a91638bfbd5d30bfcd2190815e
longvt93 <vutuanlongdhcn@gmail.com>
2022-02-28 17:36:00 +0700

    Update template upload lịch sử thanh toán

--- template/upload-pay-ctv.php
+++ template/upload-pay-ctv.php
@@ -11,8 +11,18 @@ require get_template_directory() . '/vendor/autoload.php';
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
+
+if ( ! is_user_logged_in() ) {
+	echo 'Bạn chưa đăng nhập';
+}
+
+$user_role = wp_get_current_user()->roles[0];
+if ( ! $user_role === 'administrator' ) {
+	echo 'Bạn không có quyền xem trang này';
+}
 ?>
 
+	<?php if ( is_user_logged_in() && $user_role === 'administrator' ) : ?>
 	<div class="primary">
 		<main id="main" class="site-main container" role="main">
 			<form action="" method="post" enctype="multipart/form-data">
@@ -22,28 +32,29 @@ use PhpOffice\PhpSpreadsheet\IOFactory;
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-	<?php
+		<?php
+		if ( isset( $_POST['submit'] ) ) {
+			if ( isset( $_FILES['upload'] ) ) {
 
-	if ( isset( $_POST['submit'] ) ) {
-		if ( isset( $_FILES['upload'] ) ) {
+				$file_name = $_FILES['upload']['name'];
+				$file_tmp  = $_FILES['upload']['tmp_name'];
+				$path      = WP_CONTENT_DIR . '/uploads/' . $file_name;
+				move_uploaded_file( $file_tmp, $path );
 
-			$file_name = $_FILES['upload']['name'];
-			$file_tmp  = $_FILES['upload']['tmp_name'];
-			$path      = WP_CONTENT_DIR . '/uploads/' . $file_name;
-			move_uploaded_file( $file_tmp, $path );
+				$template_path = $path;
+				if ( file_exists( $template_path ) ) {
+					$objPHPExcel = IOFactory::load( $template_path );
+					$data        = $objPHPExcel->getActiveSheet()->toArray();
+				}
+				wp_insert_pay_ctv( $data, 'wp_pay_ctv' );
 
-			$template_path = $path;
-			if ( file_exists( $template_path ) ) {
-				$objPHPExcel = IOFactory::load( $template_path );
-				$data        = $objPHPExcel->getActiveSheet()->toArray();
+				echo 'Bạn đã upload thành công';
+			} else {
+				echo 'Chưa có file nào được chọn';
 			}
-			wp_insert_pay_ctv( $data, 'wp_pay_ctv' );
-
-			echo 'Bạn đã upload thành công';
-		} else {
-			echo 'Chưa có file nào được chọn';
 		}
-	}
-	?>
+		?>
+
+	<?php endif; ?>
 <?php
 get_footer();
